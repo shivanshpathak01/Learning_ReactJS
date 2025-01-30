1660a9416d08a4d3aafde952a0f8d2e5841d31f2
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2025-01-30 11:00:37 +0000

    Some changes

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Header } from "./components/Header/Header";
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <Header/>
     </>
   )
 }
